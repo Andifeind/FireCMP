1cf95991282a6e8310770cd6447fefec74d456c4
Andifeind <andifeind@noname-media.com>
2017-01-30 21:00:45 +0100

    Add button component

---
+++ src/components/button.js
@@ -0,0 +1,48 @@
+/**
+ * Button Component
+ *
+ * Renders a button
+ *
+ * Expects:
+ * ```js
+ * {
+ *   text: 'Button text' // the button text
+ *   type: 'submit' // button type, defaults to `button`
+ * }
+ * ```
+ *
+ * @example {js}
+ * const cmp = new Button({
+ *   text: 'Submit form'
+ * });
+ *
+ * cmp.appendTo('.content');
+ *
+ * @example {html} Output:
+ * <button type="button">Submit form</button>
+ */
+
+import Core from './core';
+
+export default class Button extends Core {
+  get type() {
+    return 'button';
+  }
+
+  get enabled() {
+    return this.domEl.enabled;
+  }
+
+  set enabled(value) {
+    this.domEl.enabled = !!value;
+  }
+
+  $click(fn) {
+    this.listen('click', (ev) => {
+      fn({
+        name: ev.target.name,
+        form: ev.target.form
+      }, ev);
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import Button from './components/button';
 import Core from './components/core';
 import Input from './components/input';
 import Password from './components/input-password';
 import List from './components/list';
 
 export {
+  Button,
   Core,
   Input,
   List,
