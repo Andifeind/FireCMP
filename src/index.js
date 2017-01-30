64aa4a6ccfe85f1dfac92e22a4085091a520fb72
Andifeind <andifeind@noname-media.com>
2017-01-30 20:59:08 +0100

    Add counter component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import Core from './components/core';
+import Counter from './components/counter';
 import Input from './components/input';
 import Password from './components/input-password';
 import List from './components/list';
 
 export {
   Core,
+  Counter,
   Input,
   List,
   Password
